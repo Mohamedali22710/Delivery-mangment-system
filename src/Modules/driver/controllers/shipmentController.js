b362b02ee2c0abf9a77d51341c2d6d921467a963
Demrdash <dmrdashmhmd09@gmail.com>
2026-03-18 22:36:30 +0200

    update shipment routes and controller

--- src/Modules/driver/controllers/shipmentController.js
+++ src/Modules/driver/controllers/shipmentController.js
@@ -1,6 +1,6 @@
-const Shipment = require("../model/shipment");
+const Shipment = require("../models/shipment");
 
-// Create Shipment
+
 exports.createShipment = async (req, res) => {
   try {
     const shipment = await Shipment.create(req.body);
@@ -10,7 +10,7 @@ exports.createShipment = async (req, res) => {
   }
 };
 
-// Get all shipments
+
 exports.getAllShipments = async (req, res) => {
   try {
     const shipments = await Shipment.find();
@@ -20,11 +20,49 @@ exports.getAllShipments = async (req, res) => {
   }
 };
 
-// Get shipment by ID
+
 exports.getShipmentById = async (req, res) => {
   try {
     const shipment = await Shipment.findById(req.params.id);
     if (!shipment) return res.status(404).json({ message: "Shipment not found" });
+    res.json(shipment);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+
+exports.updateShipment = async (req, res) => {
+  try {
+    const shipment = await Shipment.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true, runValidators: true }
+    );
+
+    if (!shipment) {
+      return res.status(404).json({ message: "Shipment not found" });
+    }
+
+    res.json(shipment);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+
+exports.partialUpdateShipment = async (req, res) => {
+  try {
+    const shipment = await Shipment.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true, runValidators: true }
+    );
+
+    if (!shipment) {
+      return res.status(404).json({ message: "Shipment not found" });
+    }
+
     res.json(shipment);
   } catch (error) {
     res.status(500).json({ message: error.message });
